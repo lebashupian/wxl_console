050eae6225780a67be4497b339ea68f88217eb0a
lebashupian <lebashupian163@163.com>
2018-12-15 22:32:19 +0800

    s

--- example.rb
+++ example.rb
@@ -1,5 +1,18 @@
 #!/usr/bin/env ruby
+begin
+	puts "你可以在控制台中，查看和修改a变量的值，你会在日志中，查看到修改的效果"
+	a=1
+	Thread.new {
+		f=File.open "tmp.txt","w+"
+		f.sync = true
+		loop {
+		sleep 1
+		f.write "#{a}\n"
+		}
+	}
+	require "wxl_console"
 
-require "wxl_console"
-
-C_控制台.new.开启
+	C_控制台.new.开启
+rescue Interrupt
+	puts "\n强制中断"
+end
